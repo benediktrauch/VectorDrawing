9c7822fc13363f9262b040cae9285b9711b416fb
Nicolas Stuhlfauth <nicolas.stuhlfauth@stud.hs-ruhrwest.de>
2016-12-15 11:31:23 +0100

    Colorwidget start

--- colortoolselector.cpp
+++ colortoolselector.cpp
@@ -6,9 +6,20 @@ ColorToolSelector::ColorToolSelector(QWidget *parent) :
     ui(new Ui::ColorToolSelector)
 {
     ui->setupUi(this);
+    m_draw = new Draw();
 }
 
 ColorToolSelector::~ColorToolSelector()
 {
     delete ui;
+}
+
+void ColorToolSelector::on_pb_border_color_clicked()
+{
+
+}
+
+void ColorToolSelector::on_pb_fill_color_clicked()
+{
+
 }
--- colortoolselector.h
+++ colortoolselector.h
@@ -2,6 +2,7 @@
 #define COLORTOOLSELECTOR_H
 
 #include <QWidget>
+#include "draw.h"
 
 namespace Ui {
 class ColorToolSelector;
@@ -11,12 +12,24 @@ class ColorToolSelector : public QWidget
 {
     Q_OBJECT
 
+signals:
+    //void activeFillColorToolChanged(m_draw->fillColor);
+
+
 public:
     explicit ColorToolSelector(QWidget *parent = 0);
     ~ColorToolSelector();
 
 private:
     Ui::ColorToolSelector *ui;
+
+
+protected:
+    Draw *m_draw;
+
+private slots:
+    void on_pb_border_color_clicked();
+    void on_pb_fill_color_clicked();
 };
 
 #endif // COLORTOOLSELECTOR_H
